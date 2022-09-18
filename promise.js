38ce4cf03a4213bc04b2ccaf496d5ae1cb323a81
Feri Murdeni <fmurdeni@gmail.com>
2022-09-18 19:13:54 +0700

    Create Funsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (expression) => {
+  try {
+    // Combine films
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    const totalFilm = await theaterIXX.concat(theaterVGC);    
+
+    // Return length by the expression
+    return await totalFilm.filter((film) => film.hasil === expression).length;
+
+  } catch (error) {
+    console.log(error);
+  }
+  
+};
 
 module.exports = {
   promiseOutput,
